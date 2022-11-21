03c6d10ab3f4abd0b80b88acf96907b0da2df5d8
Toshiyuki TAKAHAGI <tosiyuki@hh.iij4u.or.jp>
2022-11-21 14:33:36 +0900

    Modelの追加と、最小限の画面構成追加

---
+++ GenericHostSample/Models/Model.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace GenericHostSample.Models;
+
+class Model
+{
+	public Model( ILogger<Model>? logger = null )
+	{
+		m_logger = logger;
+	}
+	public string FilePath { get; set; } = string.Empty;
+	public WriteableBitmap? Image { get; set; }
+
+	public void OpenFile( string filePath )
+	{
+		m_logger?.LogInformation( $"Called {System.Reflection.MethodBase.GetCurrentMethod()?.Name}( filePath: {filePath} )");
+		// ファイル名が意図したものではない場合は例外を発行
+		if( string.IsNullOrWhiteSpace( filePath ) )
+		{
+			throw new ArgumentNullException( filePath, nameof( filePath ) );
+		}
+		try
+		{
+			var decoder = BitmapDecoder.Create( new Uri( filePath, UriKind.RelativeOrAbsolute ), BitmapCreateOptions.None, BitmapCacheOption.Default );
+			Image = new WriteableBitmap( decoder.Frames[0] );
+			FilePath = filePath;
+		}
+		catch
+		{
+			FilePath = string.Empty;
+			Image = null;
+		}
+	}
+	public void CloseFile()
+	{
+		FilePath = string.Empty;
+		Image = null;
+	}
+
+	private ILogger<Model>? m_logger;
+}
--- GenericHostSample/ViewModels/MainWindowViewModel.cs
+++ GenericHostSample/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,54 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using GenericHostSample.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Controls.Ribbon;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace GenericHostSample.ViewModels;
 class MainWindowViewModel : ObservableObject
 {
 	public MainWindowViewModel()
 	{
+		Model = new Model();
 	}
+	public MainWindowViewModel( Model model )
+	{
+		Model = model;
+	}
+	public Model Model { get; set; }
+
+	public BitmapSource Image
+	{
+		// uint == 32 == 8x4 == ARGB()
+		get => Model.Image ?? BitmapSource.Create( 1, 1, 96, 96, PixelFormats.Bgra32, null, new uint[]{ 0x00000000 }, 1 );
+		set
+		{
+			// ビットマップソースから構築する
+			OnPropertyChanging();
+			Model.Image = new WriteableBitmap( value );
+			OnPropertyChanged();
+		}
+	}
+	public ICommand FileOpenCommand => m_fileOpenCommand ?? (m_fileOpenCommand = new RelayCommand( OnFileOpen ));
+
+	public ICommand FileExitCommand => m_fileExitCommand ?? (m_fileExitCommand = new RelayCommand( OnFileExit ));
+
+	private void OnFileOpen()
+	{
+		System.Windows.MessageBox.Show( "工事中...ファイル-開く" );
+	}
+	private void OnFileExit()
+	{
+		App.Current.MainWindow.Close();
+	}
+	private ICommand? m_fileOpenCommand;
+	private ICommand? m_fileExitCommand;
 }
